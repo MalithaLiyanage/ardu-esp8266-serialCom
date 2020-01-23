9af109826d07254e883e9784ef336665296a7a32
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-23 20:10:23 +0530

    Water Level Sensors Done

--- Main/GetSensors.ino
+++ Main/GetSensors.ino
@@ -1,1 +1,27 @@
+int getNutriLevelSensorVal()
+{
+  int value = digitalRead(13);  // Dummy pin number
 
+  if (value)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
+
+int getWaterLevelSensorVal()
+{
+  int value = digitalRead(12);
+
+  if (value)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
+  }
+}
