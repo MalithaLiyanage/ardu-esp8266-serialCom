e5ac5dd082f81095fc925ccd35642292c5016334
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-23 20:21:58 +0530

    pH added

--- Main/GetSensors.ino
+++ Main/GetSensors.ino
@@ -25,3 +25,50 @@ int getWaterLevelSensorVal()
     return 0;
   }
 }
+
+float getpHSensorVal()
+{
+  unsigned long int avgValue;
+  float b;
+  int buf[10], temp;
+  int sensorValue = 0;
+  float calibration = 21.34;
+
+  
+  for (int i = 0; i < 10; i++)
+  {
+    buf[i] = analogRead(4); // Dummy pin
+    delay(30);
+  }
+
+  for (int i = 0; i < 9; i++)
+  {
+    for (int j = i+1; j < 10; j++)
+    {
+      if (buf[i] > buf[j])
+      {
+        temp = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temp;
+      }
+    }
+  }
+
+  avgValue = 0;
+
+  for (int i = 2; i < 8; i++)
+  {
+    avgValue += buf[i];
+  }
+  
+  float pHVol = (float)avgValue*5.0/1024/6;
+  float phValue = -5.70 * pHVol + calibration + 11;
+
+//Serial.print("sensor = ");
+//Serial.println(phValue);
+
+return phValue;
+
+delay(500);
+  
+}
