922ec7b654369065a0617639432873b33983b1d1
Liyanage Malitha <e15299@ce.pdn.ac.lk>
2020-01-24 23:13:52 +0530

    Added Serial Communication Initialization

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,14 +1,20 @@
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+
 #include "DHT.h"
 #define DHTPIN 2     
 #define DHTTYPE DHT11 
 
 DHT dht(DHTPIN, DHTTYPE);
 
+SoftwareSerial s(5,6);
+
 
 void setup() 
 {
   
-  
+  s.begin(9600);
+  dht.begin();
 }
 
 void loop() 
